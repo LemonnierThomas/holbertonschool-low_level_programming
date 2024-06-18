0fa1c0f44051caed2e0805ce7b9ae65e5f697a8b
LemonnierThomas <thmslemonnier@gmail.com>
2024-06-18 11:11:15 +0000

    sizeof

--- hello_world/6-size.c
+++ hello_world/6-size.c
@@ -13,12 +13,12 @@ double doubleType;
 char charType;
 long longType;
 long long longlongType;
-printf("Size of int: %zu bytes\n", sizeof(intType));
-printf("Size of float: %zu bytes\n", sizeof(floatType));
-printf("Size of double: %zu bytes\n", sizeof(doubleType));
 printf("Size of char: %zu byte\n", sizeof(charType));
+printf("Size of int: %zu bytes\n", sizeof(intType));
+printf("Size of double: %zu bytes\n", sizeof(doubleType));
 printf("Size of long int: %zu byte\n", sizeof(longType));
 printf("Size of long long int: %zu byte\n", sizeof(longlongType));
+printf("Size of float: %zu bytes\n", sizeof(floatType));
 return (0);
 }
 
