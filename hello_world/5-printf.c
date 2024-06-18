81c6c3652ca18b3ed43da1690c273e28effcb572
LemonnierThomas <thmslemonnier@gmail.com>
2024-06-18 08:38:19 +0000

    printf

--- hello_world/5-printf.c
+++ hello_world/5-printf.c
@@ -4,8 +4,9 @@
  *        with proper grammar, but the outcome is a piece of art,
  * Return: 0
 */
+
 int main(void)
 {
-printf("with proper grammar, but the outcome is a piece of art,")
-return (0)
+printf("with proper grammar, but the outcome is a piece of art,");
+return (0);
 }
