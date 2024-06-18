0a4dfc76d69e886f745ecee0dbaac96ad38881eb
LemonnierThomas <thmslemonnier@gmail.com>
2024-06-18 11:03:30 +0000

    sizeof

--- hello_world/6-size.c
+++ hello_world/6-size.c
@@ -16,7 +16,7 @@ printf("Size of int: %zu bytes\n", sizeof(intType));
 printf("Size of float: %zu bytes\n", sizeof(floatType));
 printf("Size of double: %zu bytes\n", sizeof(doubleType));
 printf("Size of char: %zu byte\n", sizeof(charType));
-printf("Size of long: %zu byte/n", sizeof(longType);
+printf("Size of long: %zu byte\n", sizeof(longType);
 return (0);
 }
 
