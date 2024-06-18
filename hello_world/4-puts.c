4a2a83058c312b26be1e456c863a8f1637c11355
LemonnierThomas <thmslemonnier@gmail.com>
2024-06-18 08:26:04 +0000

    puts

--- hello_world/4-puts.c
+++ hello_world/4-puts.c
@@ -1,7 +1,13 @@
 #include <stdio.h>
-int main (void)
+/**
+ * main - program that prints exactly
+ *        "Programming is like building a multilingual puzzle
+ * Return: 0
+*/
+
+int main(void)
 {
 char *str1 = "\"Programming is like building a multilingual puzzle";
 puts(str1);
-return(0);
+return (0);
 }
