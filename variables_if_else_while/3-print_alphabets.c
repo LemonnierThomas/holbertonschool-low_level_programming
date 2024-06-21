af4504429a66afcf92197af17dc93bdc78fa2977
LemonnierThomas <thmslemonnier@gmail.com>
2024-06-21 07:33:30 +0000

    alphabets2

--- variables_if_else_while/3-print_alphabets.c
+++ variables_if_else_while/3-print_alphabets.c
@@ -11,10 +11,15 @@ int main(void)
 
 {
 	char c;
+	char d;
 	for (c = 'a'; c <= 'z'; c++)
 		{
 			putchar(c);
 		}
+		for (d = 'A'; d <= 'Z'; d++)
+		{
+			putchar(d);
+		}
 	putchar('\n');
-	return (0);
+	return 0;
 }
