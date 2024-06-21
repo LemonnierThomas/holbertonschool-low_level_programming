70711063fdec49b5680d6ab127ed3640b53354d8
LemonnierThomas <thmslemonnier@gmail.com>
2024-06-21 07:36:41 +0000

    alphabets2

--- variables_if_else_while/3-print_alphabets.c
+++ variables_if_else_while/3-print_alphabets.c
@@ -6,20 +6,19 @@
  * main - program
  * Return: 0
 */
-
 int main(void)
 
 {
-	char c;
-	char d;
-	for (c = 'a'; c <= 'z'; c++)
-		{
-			putchar(c);
-		}
-		for (d = 'A'; d <= 'Z'; d++)
-		{
-			putchar(d);
-		}
-	putchar('\n');
-	return 0;
+char c;
+char d;
+for (c = 'a'; c <= 'z'; c++)
+{
+putchar(c);
+}
+for (d = 'A'; d <= 'Z'; d++)
+{
+putchar(d);
+}
+putchar('\n');
+return (0);
 }
