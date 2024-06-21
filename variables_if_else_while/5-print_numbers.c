8d20f3be19b4d2a0209256d6dee99523bd0ea0f1
LemonnierThomas <thmslemonnier@gmail.com>
2024-06-21 09:10:35 +0000

    numbers

---
+++ variables_if_else_while/5-print_numbers.c
@@ -0,0 +1,23 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <time.h>
+
+/**
+ * main - program
+ * Return: 0
+*/
+
+int main(void)
+
+{
+	int n;
+
+	for (n = '0'; n <= '9'; n++)
+	{
+		{
+			putchar(n);
+		}
+	}
+		putchar('\n');
+	return (0);
+}
