aad549e2c350ca06a2e2c74b4589ebe9e114d55d
LemonnierThomas <thmslemonnier@gmail.com>
2024-06-21 08:28:29 +0000

    alphabt

---
+++ variables_if_else_while/4-print_alphabt.c
@@ -0,0 +1,26 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <time.h>
+
+/**
+ * main - program
+ * Return: 0
+*/
+
+int main(void)
+
+for (char c = 'a'; c <= 'Z';c++ )
+{
+if (c == 'e')
+{
+
+}
+else if (c == 'q')
+{
+;
+}
+putchar(c);
+putchar('\n');
+}
+return (0);
+}
