9db9dd8816620832300b7611c0246e31542bf1c6
LemonnierThomas <thmslemonnier@gmail.com>
2024-06-21 11:16:33 +0000

    tebahpla

---
+++ variables_if_else_while/7-print_tebahpla.c
@@ -0,0 +1,23 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <time.h>
+
+/**
+ * main - program
+ * Return: 0
+*/
+
+int main(void)
+
+{
+	char n;
+
+	for (n = 'z'; n >= 'a'; n--)
+	{
+		{
+			putchar(n);
+		}
+	}
+		putchar('\n');
+	return (0);
+}
