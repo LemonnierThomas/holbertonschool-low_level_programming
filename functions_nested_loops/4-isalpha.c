266160903460cd8a53cc2b7544d1a296599f3b79
LemonnierThomas <thmslemonnier@gmail.com>
2024-06-23 08:54:43 +0000

    isalpha

--- functions_nested_loops/4-isalpha.c
+++ functions_nested_loops/4-isalpha.c
@@ -8,7 +8,15 @@
 
 int _isalpha(int c)
 {
-	if (c >= 'a' && c <= 'z' && c >= 'A' && c <= 'Z' && c >= '0' && c <= '9')
+	if (c >= 'a' && c <= 'z')
+	{
+		return (1);
+	}
+	if (c >= 'A' && c <= 'Z')
+	{
+		return (1);
+	}
+	if (c >= '0' && c <= '9')
 	{
 		return (1);
 	}
