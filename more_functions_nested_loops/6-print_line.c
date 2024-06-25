5fb1dac009e2589ef95b03f16b0576f3f6560e20
LemonnierThomas <thmslemonnier@gmail.com>
2024-06-25 08:43:09 +0000

    print

--- more_functions_nested_loops/6-print_line.c
+++ more_functions_nested_loops/6-print_line.c
@@ -2,9 +2,16 @@
 
 /**
  * print_line - print numbers in loop
+ * @n: nombre de fois qu'il se rÃpÃte
  */
 
 void print_line(int n)
 {
-	
+	int i;
+
+	for(i = 0; i > n; i++)
+	{
+		_putchar('_');
+	}
+	_putchar('\n');
 }
