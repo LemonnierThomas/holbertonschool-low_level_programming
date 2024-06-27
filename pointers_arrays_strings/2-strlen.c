56e556c65bb230460ea9cf27c99753c83edda567
LemonnierThomas <thmslemonnier@gmail.com>
2024-06-27 12:33:36 +0000

    strlen

---
+++ pointers_arrays_strings/2-strlen.c
@@ -0,0 +1,20 @@
+#include "main.h"
+
+/**
+ * _strlen - function that return the lenght of a string.
+ * @result: result after the loop that calcul the lengt of the string
+ * return(result): return string lenght
+ */
+
+int _strlen(char *s)
+{
+	int result;
+	
+	result = 0;
+	
+	while (result <= *s)
+	{
+		result++;
+	}
+	return (result);
+}
