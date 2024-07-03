e4c8176cb8df8ede0b16688b10f689df6cb4acf9
LemonnierThomas <thmslemonnier@gmail.com>
2024-07-03 07:51:42 +0000

    locate

--- pointers_arrays_strings/1-memcpy.c
+++ pointers_arrays_strings/1-memcpy.c
@@ -1,13 +1,12 @@
 #include "main.h"
 
 /**
- * _memcpy - fills memory with a constant byte.
- * @dest: s is the pointer that point the start
- * of the block we want to fill.
- * @src: the value with which to fill the memory.
+ * _memcpy - copies memory area.
+ * @dest: pointers to buffer.
+ * @src: pointers to buffer2 + 50.
  * @n: the number of bytes.
  *
- * Return: A pointer to the memory area s.
+ * Return: return dest value.
  */
 
 char *_memcpy(char *dest, char *src, unsigned int n)
--- pointers_arrays_strings/2-strchr.c
+++ pointers_arrays_strings/2-strchr.c
@@ -4,12 +4,23 @@
  * _strchr - locates a character in a string.
  * @s: s
  * @c: c
- * @i: i
  *
  * Return: return s.
  */
 
 char *_strchr(char *s, char c)
 {
-
+	while (*s != '\0')
+	{
+		if (*s == c)
+		{
+			return (s);
+		}
+		s++;
+	}
+	if (c == '\0')
+	{
+		return (s);
+	}
+	return ('\0');
 }
