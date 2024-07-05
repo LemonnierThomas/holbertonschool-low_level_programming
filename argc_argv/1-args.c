01888d75552b9babd58dd8fdf190714ff8d82e27
LemonnierThomas <thmslemonnier@gmail.com>
2024-07-05 11:59:31 +0000

    args

---
+++ argc_argv/1-args.c
@@ -0,0 +1,17 @@
+#include "main.h"
+#include <stdio.h>
+
+/**
+ * main - main function.
+ * @argc: Argcount
+ * @argv: Argvector
+ *
+ * Return: return 0
+ */
+
+int main(int argc, char *argv[]__attribute__((unused)))
+{
+	printf("%d\n",argc);
+
+	return (0);
+}
