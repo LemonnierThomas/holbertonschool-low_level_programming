2d9a4ff5e51e2794c4ad2725fbcf2e3d6f62b15c
LemonnierThomas <thmslemonnier@gmail.com>
2024-07-12 13:12:28 +0000

    a

---
+++ more_malloc_free/0-malloc_checked.c
@@ -0,0 +1,17 @@
+#include "main.h"
+#include <stdlib.h>
+#include <stdio.h>
+#include <string.h>
+
+/**
+ * - code
+ * @grid:
+ * @height:
+ *
+ * Return:
+ */
+
+void *malloc_checked(unsigned int b)
+{
+
+}
