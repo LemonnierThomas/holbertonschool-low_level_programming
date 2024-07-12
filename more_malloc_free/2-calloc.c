df35c6c8f7b7ef07959fcd026e2af600b4a0b45d
LemonnierThomas <thmslemonnier@gmail.com>
2024-07-12 19:19:35 +0000

    calloc

--- more_malloc_free/2-calloc.c
+++ more_malloc_free/2-calloc.c
@@ -13,4 +13,23 @@
 
 void *_calloc(unsigned int nmemb, unsigned int size)
 {
+	void *ptr;
+	unsigned int n;
+
+	if (nmemb == 0 || size == 0)
+	{
+		return (NULL);
+	}
+
+	n = nmemb * size;
+	ptr = malloc(n);
+
+	if (ptr == NULL)
+	{
+		return (NULL);
+	}
+
+	memset(ptr, 0, n);
+
+	return (ptr);
 }
