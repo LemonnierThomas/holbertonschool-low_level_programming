c5fe0fcb3becfea77234757f2bf08edaeec9a79b
LemonnierThomas <thmslemonnier@gmail.com>
2024-07-12 16:09:03 +0000

    t3

---
+++ more_malloc_free/2-calloc.c
@@ -0,0 +1,16 @@
+#include "main.h"
+#include <stdlib.h>
+#include <stdio.h>
+#include <string.h>
+
+/**
+ * *_calloc - code
+ * @nmemb: a
+ * @size: a
+ *
+ * Return: ptr
+ */
+
+void *_calloc(unsigned int nmemb, unsigned int size)
+{
+}
