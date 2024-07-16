4cb2bbcdb22ed6d632451c3d15337908d5af4bca
LemonnierThomas <thmslemonnier@gmail.com>
2024-07-16 05:24:18 +0000

    printname

--- function_pointers/0-print_name.c
+++ function_pointers/0-print_name.c
@@ -1,13 +1,21 @@
-#include "main.h"
 #include <stdio.h>
+#include <stddef.h>
+#include "function_pointers.h"
 
 /**
  * print_name - function that print a name
- * @name: aa
- * @f: aa
+ * @name: name will be printed.
+ * @f: a
  */
 
 void print_name(char *name, void (*f)(char *))
 {
+	void (*p)(char *);
 
+	p = f;
+
+	if (name != NULL && p != NULL)
+	{
+		p(name);
+	}
 }
