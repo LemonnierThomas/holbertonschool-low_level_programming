dc310bd663d4e129d1f1380d73b1f1e051b243fd
LemonnierThomas <thmslemonnier@gmail.com>
2024-07-17 05:54:48 +0000

    arrayiterator

--- function_pointers/1-array_iterator.c
+++ function_pointers/1-array_iterator.c
@@ -3,10 +3,24 @@
 #include <stddef.h>
 
 /**
+ * array_iterator - aa
+ * @array: aa
+ * @size: aa
+ * @action: aa
  *
+ * return: aa
  */
 
 void array_iterator(int *array, size_t size, void (*action)(int))
 {
+	size_t i;
 
+	if (array == NULL || action == NULL)
+	{
+		return;
+	}
+	for (i = 0; i < size; ++i)
+	{
+		action(array[i]);
+	}
 }
