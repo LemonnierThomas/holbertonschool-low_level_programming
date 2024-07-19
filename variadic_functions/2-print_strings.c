4fb238095c48c8925b9165edfd03806a7e152693
root <thmslemonnier@gmail.com>
2024-07-19 08:42:05 +0000

    print string

---
+++ variadic_functions/2-print_strings.c
@@ -0,0 +1,21 @@
+#include <stdio.h>
+#include <stdarg.h>
+#include "variadic_functions.h"
+
+/**
+ * print_strings - print strings
+ * @separator: aa
+ * @n: aa
+ */
+
+void print_strings(const char *separator, const unsigned int n, ...)
+{
+	unsigned int i = 0;
+	va_list list;
+
+	va_start(list, n);
+
+	while (i < n)
+	{
+	}
+}
