d8b8c87a011699745056d2c66f049e9001a5cb04
root <thmslemonnier@gmail.com>
2024-07-19 10:50:14 +0000

    print all

---
+++ variadic_functions/3-print_all.c
@@ -0,0 +1,14 @@
+i#include <stdio.h>
+#include <stdarg.h>
+#include "variadic_functions.h"
+
+/**
+ * print_strings - print strings
+ * @separator: aa
+ * @n: aa
+ */
+
+void print_all(const char * const format, ...)
+{
+
+}
